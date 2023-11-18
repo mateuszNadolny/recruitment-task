7700fe4d25937fa0f6c49a2608e7fc2f58c93d93
mateuszNadolny <mateusznadolny00@gmail.com>
2023-11-18 15:39:47 +0100

    feat: Created custom hook to close the dropdown options whenever uses clicks outside of the dropdown itself

---
+++ src/hooks/useOutsideClick.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+
+import { UseOutsideClick } from '../lib/types';
+
+export const useOutsideClick = ({ ref, callback }: UseOutsideClick) => {
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        callback();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [ref, callback]);
+};
